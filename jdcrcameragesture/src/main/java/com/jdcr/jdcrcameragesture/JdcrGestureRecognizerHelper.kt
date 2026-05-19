c1c7e25774aec52a29fd4df75c246571ed686155
tianzhaoming <tianchaoming@codemao.cn>
2026-05-19 13:58:03 +0800

    [opt]:添加指定解析器配置

--- jdcrcameragesture/src/main/java/com/jdcr/jdcrcameragesture/JdcrGestureRecognizerHelper.kt
+++ jdcrcameragesture/src/main/java/com/jdcr/jdcrcameragesture/JdcrGestureRecognizerHelper.kt
@@ -5,6 +5,7 @@ import android.graphics.Bitmap
 import android.os.SystemClock
 import com.google.mediapipe.framework.image.BitmapImageBuilder
 import com.google.mediapipe.tasks.core.BaseOptions
+import com.google.mediapipe.tasks.core.Delegate
 import com.google.mediapipe.tasks.vision.core.RunningMode
 import com.google.mediapipe.tasks.vision.gesturerecognizer.GestureRecognizer
 import com.jdcr.jdcrcameragesture.data.JdcrGestureName
@@ -22,6 +23,8 @@ class JdcrGestureRecognizerHelper(
     private val options: Options
 ) : GestureRecognizerHelper {
 
+    @Volatile
+    private var delegateGPU = false
     private val gestureRecognizer by lazy { initGestureRecognizer() }
     private val resultFlow = MutableSharedFlow<Result<JdcrHandGestureResult>>(
         replay = 0,
@@ -32,24 +35,45 @@ class JdcrGestureRecognizerHelper(
     private val recognizer by lazy { JdcrGestureRecognizer(options) }
 
     private fun initGestureRecognizer(): GestureRecognizer {
-        val baseBuilder =
-            BaseOptions.builder().setModelAssetPath(options.modelAssetPath)
-        val optionBuilder =
-            GestureRecognizer.GestureRecognizerOptions.builder().setBaseOptions(baseBuilder.build())
-                .setRunningMode(RunningMode.LIVE_STREAM)
-                .setResultListener { mpResult, _ ->
-                    val gestureResult = recognizer.processRecognitionResult(mpResult)
-                    if (gestureResult.isSuccess) {
-                        JdcrGestureLog.i("异步识别结果:" + gestureResult.getOrNull())
+        fun createFromOptions(baseOptions: BaseOptions): GestureRecognizer {
+            val optionBuilder =
+                GestureRecognizer.GestureRecognizerOptions.builder().setBaseOptions(baseOptions)
+                    .setRunningMode(RunningMode.LIVE_STREAM)
+                    .setResultListener { mpResult, _ ->
+                        val gestureResult = recognizer.processRecognitionResult(mpResult)
+                        if (gestureResult.isSuccess) {
+                            JdcrGestureLog.i("异步识别结果:" + gestureResult.getOrNull())
+                        }
+                        resultFlow.tryEmit(gestureResult)
                     }
-                    resultFlow.tryEmit(gestureResult)
+                    .setNumHands(options.maxHand)
+                    .setErrorListener {
+                        JdcrGestureLog.e("异步识别异常", it)
+                    }
+            JdcrGestureLog.i("创建手势识别处理器")
+            return GestureRecognizer.createFromOptions(context, optionBuilder.build())
+        }
+        try {
+            val baseBuilder =
+                BaseOptions.builder().setModelAssetPath(options.modelAssetPath).apply {
+                    if (options.delegate != null) {
+                        JdcrGestureLog.i("指定解析器为:${options.delegate}")
+                        setDelegate(options.delegate)
+                        if (options.delegate == Delegate.GPU) {
+                            delegateGPU = true
+                        }
+                    }
                 }
-                .setNumHands(options.maxHand)
-                .setErrorListener {
-                    JdcrGestureLog.e("异步识别异常", it)
+            return createFromOptions(baseBuilder.build())
+        } catch (e: Exception) {
+            JdcrGestureLog.e("创建手势识别处理器异常,回退cpu解析器", e)
+            val baseBuilder =
+                BaseOptions.builder().setModelAssetPath(options.modelAssetPath).apply {
+                    setDelegate(Delegate.CPU)
+                    delegateGPU = false
                 }
-        JdcrGestureLog.i("创建手势识别处理器")
-        return GestureRecognizer.createFromOptions(context, optionBuilder.build())
+            return createFromOptions(baseBuilder.build())
+        }
     }
 
     private fun recognizeLiveStream(bitmap: Bitmap) {
@@ -71,12 +95,21 @@ class JdcrGestureRecognizerHelper(
         return resultFlow
     }
 
+    fun isDelegateGPU(): Boolean {
+        return delegateGPU
+    }
+
+    fun close() {
+        gestureRecognizer.close()
+    }
+
     class Options internal constructor(
         val modelAssetPath: String,
         val maxHand: Int,
         val modelMatchScore: Float,
         val allowGestures: Set<String>,
-        val customRecognizer: CustomRecognizer?
+        val customRecognizer: CustomRecognizer?,
+        val delegate: Delegate?,
     )
 
     class Builder {
@@ -86,6 +119,7 @@ class JdcrGestureRecognizerHelper(
         private var modelMatchScore = 0.6f
         private var allowGestures = JdcrGestureName.CMT
         private var customRecognizer: CustomRecognizer? = JdcrGestureCustomRecognizer()
+        private var delegate: Delegate? = null
 
         fun setModelAssetPath(path: String): Builder {
             this.modelAssetPath = path
@@ -112,6 +146,11 @@ class JdcrGestureRecognizerHelper(
             return this
         }
 
+        fun setDelegate(delegate: Delegate?): Builder {
+            this.delegate = delegate
+            return this
+        }
+
         fun build(): Options {
             requireNotNull(modelAssetPath)
             return Options(
@@ -119,7 +158,8 @@ class JdcrGestureRecognizerHelper(
                 maxHand,
                 modelMatchScore,
                 allowGestures,
-                customRecognizer
+                customRecognizer,
+                delegate
             )
         }
 
